4fd990957ea1cd5dda28eea05266234b3d922af3
Raul Gonzalez <raulg42@gmail.com>
2019-03-08 14:25:00 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+const Index = () => <div>Index.js</div>;
+
+export default Index;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready http://localhost:3000 <');
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
