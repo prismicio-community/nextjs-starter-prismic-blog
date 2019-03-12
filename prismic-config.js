27a294b1988b89e943283a34c64dbdc2c0a06814
Raul Gonzalez <raulg42@gmail.com>
2019-03-12 17:34:36 +0100

    Demo repository

--- prismic-config.js
+++ prismic-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiEndpoint: 'https://bambi-blog.prismic.io/api/v2',
+  apiEndpoint: 'https://react-blog-demo.prismic.io/api/v2',
   linkResolver: function(doc) {
     if (doc.type === 'post') {
       return `/blog/${doc.uid}`;
