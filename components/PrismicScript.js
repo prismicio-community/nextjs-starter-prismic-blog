b777dc0dd6753e7b01d33fa3fd42f9e07e77757a
PaulinaVPG <pvpg@live.com.mx>
2020-09-08 21:48:23 -0500

    re added new script

--- components/PrismicScript.js
+++ components/PrismicScript.js
@@ -3,7 +3,7 @@ import { apiEndpoint } from 'prismic-configuration'
 const PrismicScript = () => {
   const [, repoName] = apiEndpoint.match(/https?:\/\/([^.]+)?\.(cdn\.)?.+/);
   return (
-    <script async defer type="text/javascript" src={`"https://static.cdn.prismic.io/prismic.min.js?repo=${repoName}&new=true"`} />
+    <script async defer src={`"https://static.cdn.prismic.io/prismic.min.js?repo=${repoName}&new=true"`} />
   )
 }
 
