2ad7296f818a7d4500ed7e81e37a90562fd837d9
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-03-03 15:15:36 -1000

    refactor: use `components` export from Slice Machine

--- pages/blog/[uid].js
+++ pages/blog/[uid].js
@@ -4,7 +4,7 @@ import { SliceZone } from "@prismicio/react";
 import * as prismicH from "@prismicio/helpers";
 
 import { createClient, linkResolver } from "../../prismicio";
-import { components } from "../../slices/components";
+import { components } from "../../slices";
 
 import { Layout } from "../../components/Layout";
 import { BackButton } from "../../components/BackButton";
