6c5d4fd2b985bce7df6308fead49e250d462eff5
phillysnow <47355806+phillysnow@users.noreply.github.com>
2022-03-10 19:07:02 +0100

    Update slice-simulator.jsx

--- pages/slice-simulator.jsx
+++ pages/slice-simulator.jsx
@@ -1,7 +1,7 @@
 import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../slices/components";
+import { components } from "../slices/index";
 import state from "../.slicemachine/libraries-state.json";
 
 const SliceSimulatorPage = () => {
