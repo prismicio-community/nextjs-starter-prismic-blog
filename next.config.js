96eb007b266120a1622d5d1feedc241f00f9a6a7
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-04-12 10:52:10 -1000

    feat: blank create-next-app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const path = require("path");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = {
-  webpack(config) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
-  images: {
-    domains: ["images.prismic.io", "images.unsplash.com"],
-  },
-};
+module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
-import Head from "next/head";
-import Link from "next/link";
-import { PrismicProvider } from "@prismicio/react";
-import { PrismicPreview } from "@prismicio/next";
+import '../styles/globals.css'
 
-import { repositoryName, linkResolver } from "../prismicio";
-
-import "../styles.css";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <PrismicProvider
-      linkResolver={linkResolver}
-      internalLinkComponent={({ href, children, ...props }) => (
-        <Link href={href}>
-          <a {...props}>
-            {children}
-          </a>
-        </Link>
-      )}
-    >
-      <PrismicPreview repositoryName={repositoryName}>
-        <Component {...pageProps} />
-      </PrismicPreview>
-    </PrismicProvider>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
