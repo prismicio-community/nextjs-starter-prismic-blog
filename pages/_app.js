7904830057e68bfddb4f288f03b5ed134c06c180
Sam Littlefair <sam.littlefair@prismic.io>
2022-06-28 15:15:46 -0300

    chore: add documentation banner

--- pages/_app.js
+++ pages/_app.js
@@ -69,6 +69,32 @@ export default function App({ Component, pageProps }) {
       internalLinkComponent={NextLinkShim}
       richTextComponents={richTextComponents}
     >
+      {/* TODO: Remove the following element once you have read the documentation. */}
+      {process.env.NODE_ENV === "development" && (
+        <div
+          style={{
+            background: "#5163ba",
+            padding: "1rem",
+            textAlign: "center",
+            fontSize: "0.85rem",
+            color: "#fff",
+          }}
+        >
+          <p>
+            <strong>👋 Welcome to your new website!</strong> To customize the
+            code and content of this site,{" "}
+            <a
+              href="https://github.com/prismicio-community/nextjs-starter-prismic-multi-language/tree/master/docs"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "underline" }}
+            >
+              see the documentation
+            </a>
+            . Remove this bar in <code>pages/_app.js</code>.
+          </p>
+        </div>
+      )}
       <PrismicPreview repositoryName={repositoryName}>
         <Component {...pageProps} />
       </PrismicPreview>
