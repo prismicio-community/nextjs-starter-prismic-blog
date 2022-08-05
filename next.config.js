a3f40f0b7d1388761d229f13846a0f49d9bb7893
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-08-05 11:20:35 -1000

    chore(deps): upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "imgix",
-    path: "",
-    domains: ["images.prismic.io"],
-  },
   async rewrites() {
     return [
       {
