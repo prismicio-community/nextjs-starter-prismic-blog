7fd32b2d8b1ab86047e3058a6331ad210ec0f353
Pau <32859437+pvpg@users.noreply.github.com>
2023-01-05 17:34:06 -0600

    chore: update to Slice Machine v0.6.0 (#102)

--- pages/slice-simulator.js
+++ pages/slice-simulator.js
@@ -1,23 +1,22 @@
-import { SliceSimulator } from "@prismicio/slice-simulator-react";
-import { SliceZone } from "@prismicio/react";
+import { SliceSimulator } from "@prismicio/slice-simulator-react"
+import { SliceZone } from "@prismicio/react"
 
-import state from "../.slicemachine/libraries-state.json";
-import { components } from "../slices";
+import { components } from "../slices"
 
 const SliceSimulatorPage = () => (
   <SliceSimulator
     // The "sliceZone" prop should be a function receiving slices and rendering them using your "SliceZone" component.
     sliceZone={(props) => <SliceZone {...props} components={components} />}
-    state={state}
+    state={{}}
   />
-);
+)
 
-export default SliceSimulatorPage;
+export default SliceSimulatorPage
 
 export const getStaticProps = async () => {
   if (process.env.NODE_ENV === "production") {
-    return { notFound: true };
+    return { notFound: true }
   } else {
-    return { props: {} };
+    return { props: {} }
   }
-};
+}
