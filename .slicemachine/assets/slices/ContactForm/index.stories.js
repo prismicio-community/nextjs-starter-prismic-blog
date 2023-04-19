1db6fbefcaee374226fd6dac2b70addcbc6d6c42
Hugo Villain <hugo.p.villain@gmail.com>
2023-04-19 12:11:38 +0200

    plugins

--- .slicemachine/assets/slices/ContactForm/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/ContactForm';
-
-export default {
-  title: 'slices/ContactForm'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"contact_form","items":[],"primary":{},"id":"_Default"}} />
-_Default.storyName = 'Default'
