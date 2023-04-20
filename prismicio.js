b43c146f6a1588444c2b19808849c7458ebc8c8f
Hugo Villain <hugo.p.villain@gmail.com>
2023-04-20 10:19:41 +0200

    export

--- prismicio.js
+++ prismicio.js
@@ -6,7 +6,7 @@ import sm from "./slicemachine.config.json";
 /**
  * The project's Prismic repository name.
  */
-const { repositoryName } = sm;
+export const { repositoryName } = sm;
 
 /**
  * The project's Prismic Route Resolvers. This list determines a Prismic document's URL.
@@ -40,5 +40,3 @@ export const createClient = ({ previewData, req, ...config } = {}) => {
 
   return client;
 };
-
-export { repositoryName };
