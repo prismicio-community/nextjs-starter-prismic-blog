aa177efa289db0cfc2efdf0a1b9a1156a4dc892a
Rafael Paz <rafael@collato.com>
2023-05-01 09:48:55 +0200

    Add prettier to eslint config and update PortfolioVideo file formatting

--- components/Home/Portfolio/PortfolioVideo.tsx
+++ components/Home/Portfolio/PortfolioVideo.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import YouTube from 'react-youtube';
-import { isMobile } from 'react-device-detect';
+import React from "react";
+import PropTypes from "prop-types";
+import YouTube from "react-youtube";
+import { isMobile } from "react-device-detect";
 
 const PortfolioVideo: React.FC<{ videoId: string }> = ({ videoId }) => {
   const opts = {
@@ -11,16 +11,9 @@ const PortfolioVideo: React.FC<{ videoId: string }> = ({ videoId }) => {
   if (isMobile) opts.width = window.innerWidth - 20;
   return (
     <div className="col-md-4 col-sm-6 portfolio-b">
-      <YouTube
-        videoId={videoId}
-        opts={opts}
-      />
+      <YouTube videoId={videoId} opts={opts} />
     </div>
   );
-};
-
-PortfolioVideo.propTypes = {
-  videoId: PropTypes.string.isRequired,
 };
 
 export default PortfolioVideo;
