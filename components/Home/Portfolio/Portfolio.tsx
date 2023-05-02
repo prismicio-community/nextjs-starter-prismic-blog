68934f170fe423e53c8d33a202e53e4899149e80
Rafael Paz <rafael@collato.com>
2023-05-02 14:03:43 +0200

    Refactor Portfolio component video display

--- components/Home/Portfolio/Portfolio.tsx
+++ components/Home/Portfolio/Portfolio.tsx
@@ -12,17 +12,17 @@ const Portfolio: React.FC = () => (
           </div>
         </div>
       </div>
-      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+      <div className="container mx-auto py-2 lg:pt-12">
         <div className="-m-1 flex flex-wrap md:-m-2">
           <PortfolioImage imgName="gallery_image" alt="First Image" />
           <PortfolioImage imgName="p2" alt="Second Image" />
           <PortfolioImage imgName="p3" alt="Third Image" />
           <PortfolioVideo videoId="lUaMXm_DLJ4" />
-        <PortfolioVideo videoId="GNFQ8WzHc1w" />
-        <PortfolioVideo videoId="IB2v9tw7q_g" />
-        <PortfolioVideo videoId="4eNeaim4VRQ" />
-        <PortfolioVideo videoId="yDeH1W2eshQ" />
-        <PortfolioVideo videoId="qSgojcFYJ1Q" />
+          <PortfolioVideo videoId="GNFQ8WzHc1w" />
+          <PortfolioVideo videoId="IB2v9tw7q_g" />
+          <PortfolioVideo videoId="4eNeaim4VRQ" />
+          <PortfolioVideo videoId="yDeH1W2eshQ" />
+          <PortfolioVideo videoId="qSgojcFYJ1Q" />
         </div>
       </div>
     </div>
