ad50e7e1db9c18024bfdc60dd044f1a2a5293ffe
Rafael Paz <rafael@collato.com>
2023-05-07 23:27:25 +0200

    Add photoswipe and react-photoswipe-gallery dependencies

--- src/components/Home/Portfolio/Portfolio.tsx
+++ src/components/Home/Portfolio/Portfolio.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import PortfolioImage from "./PortfolioImage";
 import PortfolioVideo from "./PortfolioVideo";
 import "twin.macro";
+import "photoswipe/dist/photoswipe.css";
 
+import { Gallery, Item } from "react-photoswipe-gallery";
+
+// https://res.cloudinary.com/dadaboom/image/upload/c_scale,h_335,w_424/v1530620438/gallery_image.jpg
 const Portfolio: React.FC = () => (
   <section id="portfolio" className="portfolio-section">
     <div className="container">
@@ -14,9 +18,54 @@ const Portfolio: React.FC = () => (
         </div>
       </div>
       <div tw="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 auto-rows-fr">
-        <PortfolioImage imgName="gallery_image" alt="First Image" />
-        <PortfolioImage imgName="p2" alt="Second Image" />
-        <PortfolioImage imgName="p3" alt="Third Image" />
+        <Gallery id="portfolio-gallery">
+          <Item
+            original="https://res.cloudinary.com/dadaboom/image/upload/c_crop,h_800,w_1000/v1530620438/gallery_image.jpg"
+            thumbnail="https://res.cloudinary.com/dadaboom/image/upload/c_thumb,h_335,w_424/v1530620438/gallery_image.jpg"
+            width="1000"
+            height="800"
+          >
+            {({ ref, open }) => (
+              <img
+                ref={ref}
+                onClick={open}
+                src="https://res.cloudinary.com/dadaboom/image/upload/c_thumb,h_335,w_424/v1530620438/gallery_image.jpg"
+                tw="cursor-pointer"
+              />
+            )}
+          </Item>
+          <Item
+            original="https://res.cloudinary.com/dadaboom/image/upload/v1530620438/p2.jpg"
+            thumbnail="https://res.cloudinary.com/dadaboom/image/upload/c_scale,h_335,w_424/v1530620438/p2.jpg"
+            width="640"
+            height="506"
+          >
+            {({ ref, open }) => (
+              <img
+                ref={ref}
+                onClick={open}
+                src="https://res.cloudinary.com/dadaboom/image/upload/c_scale,h_335,w_424/v1530620438/p2.jpg"
+                tw="cursor-pointer"
+              />
+            )}
+          </Item>
+          <Item
+            original="https://res.cloudinary.com/dadaboom/image/upload/v1530620438/p3.jpg"
+            thumbnail="https://res.cloudinary.com/dadaboom/image/upload/c_scale,h_335,w_424/v1530620438/p3.jpg"
+            width="370"
+            height="247"
+            tw="cursor-pointer"
+          >
+            {({ ref, open }) => (
+              <img
+                ref={ref}
+                onClick={open}
+                src="https://res.cloudinary.com/dadaboom/image/upload/c_scale,h_335,w_424/v1530620438/p3.jpg"
+                tw="cursor-pointer"
+              />
+            )}
+          </Item>
+        </Gallery>
         <PortfolioVideo videoId="lUaMXm_DLJ4" />
         <PortfolioVideo videoId="GNFQ8WzHc1w" />
         <PortfolioVideo videoId="IB2v9tw7q_g" />
