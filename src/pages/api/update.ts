4683aa4d3a9db0f93858dc4b7de2da2b1619d88e
Rafael Paz <rafael@collato.com>
2023-05-08 15:34:21 +0200

    edit console

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -4,7 +4,7 @@ const update = async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.dir("@@ req", req);
+  console.dir(`@@ req: ${req.body}`);
   res.status(200).json({
     message: "OK",
   });
