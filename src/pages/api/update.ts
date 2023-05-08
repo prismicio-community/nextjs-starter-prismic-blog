4363164d0d26f7ec2099ec5dc0f4fab357a4e1a1
Rafael Paz <rafael@collato.com>
2023-05-08 15:26:35 +0200

    send response

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -2,9 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const update = async function handler(
   req: NextApiRequest,
-  _res: NextApiResponse
+  res: NextApiResponse
 ) {
   console.dir("req", req.body);
+  res.status(200).json({
+    message: "OK",
+  });
 };
 
 export default update;
