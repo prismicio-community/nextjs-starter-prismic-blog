f8fad1c493edcf5b9fe873552744913a5c9059c2
Rafael Paz <rafael@collato.com>
2023-05-08 15:47:44 +0200

    add secret

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -4,6 +4,12 @@ const update = async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { body } = req;
+  if (!body) return res.json({ revalidated: false });
+
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_KEY) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
   console.dir(req.body, { depth: null });
   res.status(200).json({
     message: "OK",
@@ -11,29 +17,3 @@ const update = async function handler(
 };
 
 export default update;
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const { body } = req;
-//   if (!body) return res.json({ revalidated: false });
-
-//   // Check for secret to confirm this is a valid request
-//   // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-//   //   return res.status(401).json({ message: 'Invalid token' });
-//   // }
-
-//   try {
-//     // this should be the actual path not a rewritten path
-//     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-//     const url = getUrlByType(body._type, body.slug?.current);
-
-//     await res.revalidate(url);
-//     return res.json({ revalidated: true });
-//   } catch (err) {
-//     // If there was an error, Next.js will continue
-//     // to show the last successfully generated page
-//     return res.status(500).send('Error revalidating');
-//   }
-// }
