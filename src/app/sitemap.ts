a4aaf75574c981662765c1d208e2a3fa5f1d19ff
Rafael Paz <rafael@collato.com>
2023-06-01 22:41:08 +0200

    Add revalidate route and update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,19 @@
 import { MetadataRoute } from "next";
 import { createClient } from "@/prismicio";
 
+const BASE_URL = "https://dadaboom.co.il";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const client = createClient();
   const homepage = await client.getSingle("homepage");
   const posts = await client.getAllByType("post");
   return [
     {
-      url: "/",
+      url: BASE_URL,
       lastModified: homepage.last_publication_date,
     },
     ...posts.map((post) => ({
-      url: `https://dadaboom.co.il/blog/${post.uid}`,
+      url: `${BASE_URL}/blog/${post.uid}`,
       lastModified: post.last_publication_date,
     })),
   ];
--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@/prismicio";
 
 const update = async function handler(
   req: NextApiRequest,
@@ -7,14 +8,19 @@ const update = async function handler(
   const { body } = req;
   console.log("Body:");
   console.dir(body, { depth: null });
-  if (!body) return res.json({ revalidated: false });
+  // if (!body) return res.json({ revalidated: false });
 
-  if (req.body.secret !== process.env.REVALIDATE_SECRET_KEY) {
-    console.log("Invalid secret key");
-    return res.status(401).json({ message: "Invalid token" });
-  }
+  // if (req.body.secret !== process.env.REVALIDATE_SECRET_KEY) {
+  //   console.log("Invalid secret key");
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
 
   console.log("before revalidate");
+  const client = createClient();
+  const posts = await client.getAllByType("post");
+  console.log("@@@ ~ posts:", posts);
+  const doc = await client.getByID("YisQUREAAC0AQX5X");
+  console.log("@@@ ~ doc:", doc);
   await res.revalidate("/");
   console.log("after revalidate");
   return res.json({ revalidated: true });
