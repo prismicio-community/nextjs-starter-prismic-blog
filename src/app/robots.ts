57e3ddce52de99f952421e0da0a035b7e46677ee
Rafael Paz <rafael@collato.com>
2023-06-01 20:45:58 +0200

    ✨ feat(robots.ts): add robots.txt file with allow all user agents and sitemap url.

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://dadaboom.co.il/sitemap.xml",
+  };
+}
