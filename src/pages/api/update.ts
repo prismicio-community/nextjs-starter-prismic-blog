7e0dfb7aa3c3c08abc6ae8b833ea6e7b29391caf
Rafael Paz <rafael@collato.com>
2023-06-02 13:23:20 +0200

    Refactor update API for error handling

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -4,20 +4,27 @@ const update = async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { body } = req;
-  console.log("Body:");
-  console.dir(body, { depth: null });
-  if (!body) return res.json({ revalidated: false });
+  try {
+    const { body } = req;
+    console.log("Body:");
+    console.dir(body, { depth: null });
+    if (!body) return res.json({ revalidated: false });
 
-  if (req.body.secret !== process.env.REVALIDATE_SECRET_KEY) {
-    console.log("Invalid secret key");
-    return res.status(401).json({ message: "Invalid token" });
+    if (req.body.secret !== process.env.REVALIDATE_SECRET_KEY) {
+      console.log("Invalid secret key");
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    console.log("before revalidate");
+    await res.revalidate("/");
+    console.log("after revalidate");
+    return res.json({ revalidated: true });
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+    return res.status(500).json({ message: "Unknown error" });
   }
-
-  console.log("before revalidate");
-  await res.revalidate("/");
-  console.log("after revalidate");
-  return res.json({ revalidated: true });
 };
 
 export default update;
